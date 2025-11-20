84c6155904b879dbebacca4ae398f70bde5c4676
Chris Roadfeldt <chris@roadfeldt.com>
2025-11-19 23:02:36 -0600

    FUnctional code from Works branch

--- filter_plugins/subnet_filter.py
+++ filter_plugins/subnet_filter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # Custom Ansible Filter to implement the subnet generation logic
 
+from ansible.errors import AnsibleFilterError
 # Import ipaddress module for robust IP handling in Python
 import ipaddress 
 
@@ -18,8 +19,7 @@ def generate_subnet_details_filter(team_number_input):
         team_num_str = str(team_number)
         
         if not 1 <= len(team_num_str) <= 5:
-            # Returning None instead of raising an AnsibleFilterError
-            return None
+            raise ValueError(f"Team number {team_number} has an invalid length.")
 
         second_octet = 0
         third_octet = 0
@@ -62,3 +62,4 @@ class FilterModule(object):
         return {
             'generate_subnet_details_filter': generate_subnet_details_filter
         }
+
